1cf54f17ce40beeadb4634b236a0427238d67d41
Rodolfo Strunkis <rmstrunkis1@gmail.com>
2021-11-16 19:41:31 -0300

    Domain Categoria Criado e Resource Categoria Alterado para List

--- src/main/java/com/rodolfo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rodolfo/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.rodolfo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.rodolfo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(categoria1);
+		listaCategoria.add(categoria2);
+		
+		return listaCategoria;
 	}
 
 }
